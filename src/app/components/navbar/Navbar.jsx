f42ae67ddd08d98d9423a1d7641755ca7accfe71
damialade <omoladedamilola1989@gmail.com>
2024-12-02 19:56:49 +0100

    cv link updated

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
           <FaLinkedinIn />
         </Link>
         <Link
-          href="https://drive.google.com/file/d/1GkxdDDlYfoWdsHfKn6Ttuorqcu5kuYPN/view?usp=sharing"
+          href="https://drive.google.com/file/d/1FskMPHDJnvcGWLm25GzMTrTl9P30ZVD5/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
